66eaf0543f292e2d52246ffbd85d3e935fcb5301
birichani-codes <birichani.solomon@gmail.com>
2024-05-05 17:41:54 +0300

    tests add

--- scripts.test.js
+++ scripts.test.js
@@ -9,6 +9,7 @@ dbMock =[
 
 ];
 
+describe('googleSearch',()=>{
 
 it('this is this the test.',()=>{
 expect ('Hello').toBe('Hello')
@@ -18,4 +19,12 @@ it('is searching google test', ()=>{
 expect(googleSearch('testtest',dbMock)).toEqual([])
 expect(googleSearch('dog',dbMock)).toEqual(['dog.com','dogpictures.com'])
 
+})
+it('work with undefined and null input',()=>{
+expect(googleSearch(undefined,dbMock)).toEqual([])
+expect(googleSearch(null,dbMock)).toEqual([])
+})
+it('does not return more than 4 matches',()=>{
+expect(googleSearch('.com',dbMock).length).toEqual(4);
+})
 })
